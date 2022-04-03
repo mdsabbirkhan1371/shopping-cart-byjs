c928b4a8ac273954eb53f41502a3b3da3fab7e62
mdsabbirkhan1371 <mdsabbirkhan1972@gmail.com>
2022-04-03 16:13:27 -0400

    second

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-document.getElementById('silicon-button').addEventListener('click', function () {
-    const caseInput = document.getElementById('silicon-input');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+function productCaseNumber(product, price, isIncrasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncrasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+
+    // update case total 
+
+    const productTotalPrice = document.getElementById(product + '-total');
+    productTotalPrice.innerText = productNumber * price;
+}
+// handle phone increase and decrease event 
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    productCaseNumber('phone', 1219, true)
+})
+
+document.getElementById('phone-minus').addEventListener('click', function () {
+    productCaseNumber('phone', 1219, false)
+})
+
+
+// handle case increase and decrease event 
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    productCaseNumber('case', 59, true);
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    productCaseNumber('case', 59, false)
+
 })
